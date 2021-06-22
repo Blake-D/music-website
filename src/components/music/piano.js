6d7cf6bed538359d99a9e9d899daa9d16f24bf26
Blake DeGraw <bbdegraw@gmail.com>
2021-06-22 13:01:54 -0700

    all piano and most string tracks linked and playing successfully. ran into a duration limit with diler that I need to address for future tracks

--- src/components/music/piano.js
+++ src/components/music/piano.js
@@ -4,12 +4,31 @@ import Nav from '../nav'
 function Piano(){
     return(
         <div>
-            {/* <Nav />
+            <Nav />
             <h1>Piano music</h1>
             <p>We Grant Thee Our Bodies amidst Spiritual Decline</p>
             <audio src="https://docs.google.com/uc?export=open&id=1eqesg77hkTl06YDoLfveRNmlAQH7PIlt" controls controlsList="nodownload"/>
             <p>Two Chromatic Miniatures</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1KFPnvgs-rnu0UYIxpgi8aXsJZCwK5Bdk" controls/> */}
+            <audio src="https://docs.google.com/uc?export=download&id=1KFPnvgs-rnu0UYIxpgi8aXsJZCwK5Bdk" controls controlsList="nodownload"/>
+            <p>The Conqueror Snail (four hands)</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1N3FVE310B7B0tWFkVo1wP1yIK3m5WTIv" controls controlsList="nodownload"/>
+            <p>Microgressive Isotopic Fractionations/Prolonged Exposure</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1HKSivNE8mZMOD2D9tB0Q0zys-0M-5kXt" controls controlsList="nodownload"/>
+            <p>Matrixial Decomposition</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1kKELTqbpk6FIpH8bJzn6tO7zLq3qfErN" controls controlsList="nodownload"/>
+            <p>Lagomorphic Pronunciation (for four pianos)</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1r6D7lWtxgwJ85KOIUioxGUxll7_Ydka5" controls controlsList="nodownload"/>
+            <p>Houndstooth (for four pianos)</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1uAVjHCq_OOlwAN9GU3TgJlO3xaqTM2vg" controls controlsList="nodownload"/>
+            <p>Fugue</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1a57SXJSjWxbCS7JdbLDm9IKPz8yPqev2" controls controlsList="nodownload"/>
+            <p>Eleven Etudes</p>
+            <audio src="https://docs.google.com/uc?export=download&id=12ybz68TnpsBRZ8mpv3oyzLUCJK92gLtG" controls controlsList="nodownload"/>
+            <p>Dirge</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1P66PgzzBU2_-4Tt1KLd9AGKzjY1Vf1eV" controls controlsList="nodownload"/>
+            <p>[having to do with] Limbed Creatures in a Tidepool (four hands)</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1h1GFAYNOmxqck_ztZwR4NRBtHbkyWcmf" controls controlsList="nodownload"/>
+            
         </div>
     )
 }
--- src/components/music/strings.js
+++ src/components/music/strings.js
@@ -6,6 +6,25 @@ function Strings(){
         <div>
             <Nav />
             <h1>String music here</h1>
+            <p>Triple Quartet</p>
+            <audio src="https://docs.google.com/uc?export=download&id=17yX5FCmg4lEsOtdqXNdTrvxMWPjbShWS" controls controlsList="nodownload"/>
+            <p>The Thrombosing</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1q-KVoOTtePTWX7yB0Uwmh8vBSaHwY2qW" controls controlsList="nodownload"/>
+            <p>Slath</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1IoMdq46J50TZZxak23K-TaZq6QRlN8l7" controls controlsList="nodownload"/>
+            <p>Skin Procession</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1G4rf_-90Kccj3YDBs3cqegebRBVD9JUj" controls controlsList="nodownload"/>
+            <p>Prodromal Canon</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1dAlisB9_J3GRGrYSZeQE1RoyVaXCzpjG" controls controlsList="nodownload"/>
+            <p>Near'r</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1Hln1om1ct4kz1wpqqA1qT4TXLIwk6qBZ" controls controlsList="nodownload"/>
+            <p>Mutual Intelligibility</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1SEMwMCuWdOZD3852aj4qgIoj0LcTyg13" controls controlsList="nodownload"/>
+            <p>Electronic Quintet for Humans</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1-Pqgi4_-dnbcuTUfS0Q_PafvGcjDUsbu" controls controlsList="nodownload"/>
+            {/* <p>Diler</p>
+            <audio src="https://docs.google.com/uc?export=download&id=1yHl_QytRzsV0Rayd5b7TTjCPc1b-5iGr" controls controlsList="nodownload"/> */}
+            
         </div>
     )
 }
