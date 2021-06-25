2606f3ac56171ddb3210ed2faf810ea164838188
Blake DeGraw <bbdegraw@gmail.com>
2021-06-25 11:25:11 -0700

    got basic modal working on App level

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Modal from 'react-modal'
 import './styles.css'
 import Home from './components/home/home'
 import About from './components/about'
@@ -11,19 +12,40 @@ import Vocal from './components/music/vocal'
 import Electronic from './components/music/electronic'
 import ChamberMisc from './components/music/chamber-misc'
 
+Modal.setAppElement('#root')
+
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   return (
     <div>
+      <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
+      <Modal 
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={
+          {
+            overlay: {
+              backgroundColor: 'gray'
+            },
+            content: {
+              color: 'orange'
+            }
+          }
+        }
+        >
+        <h2>Modal Title</h2>
+        <button onClick={() => setModalIsOpen(false)}>close</button>
+      </Modal>
       <Router>
-        <Route exact path="/" render={Home}/>
-        <Route exact path="/about" render={About}/>
-        <Route exact path="/collab" render={Collab}/>
-        <Route exact path="/music" render={Music}/>
-        <Route exact path="/music/piano" render={Piano}/>
-        <Route exact path="/music/Strings" render={Strings}/>
-        <Route exact path="/music/vocal" render={Vocal}/>
-        <Route exact path="/music/electronic" render={Electronic}/>
-        <Route exact path="/music/chamber-misc" render={ChamberMisc}/>
+        <Route exact path="/" render={Home} />
+        <Route exact path="/about" render={About} />
+        <Route exact path="/collab" render={Collab} />
+        <Route exact path="/music" render={Music} />
+        <Route exact path="/music/piano" render={Piano} />
+        <Route exact path="/music/Strings" render={Strings} />
+        <Route exact path="/music/vocal" render={Vocal} />
+        <Route exact path="/music/electronic" render={Electronic} />
+        <Route exact path="/music/chamber-misc" render={ChamberMisc} />
       </Router>
     </div>
   )
--- src/components/music/vocal.js
+++ src/components/music/vocal.js
@@ -1,26 +1,25 @@
-import React from 'react'
 import Nav from '../nav'
 
-function Vocal(){
-    return(
+function Vocal() {
+    return (
         <div>
             <Nav />
             <p>Victims of the Rooster</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1H0Z7yH_fMVg4hf0cLDwh8Oo5VuXUGijL" controls controlsList="nodownload"/>
+            <audio src="https://docs.google.com/uc?export=download&id=1H0Z7yH_fMVg4hf0cLDwh8Oo5VuXUGijL" controls controlsList="nodownload" />
             <p>They</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1FAfS9VVeD1HiKBuZ-bKytR9W8-YkCmzW" controls controlsList="nodownload"/>
+            <audio src="https://docs.google.com/uc?export=download&id=1FAfS9VVeD1HiKBuZ-bKytR9W8-YkCmzW" controls controlsList="nodownload" />
             <p>Six Vowels</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1ikr3KtRIds5kw0iYhC0rM5zw2170VS7V" controls controlsList="nodownload"/>
+            <audio src="https://docs.google.com/uc?export=download&id=1ikr3KtRIds5kw0iYhC0rM5zw2170VS7V" controls controlsList="nodownload" />
             <p>Second Draft</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1pMxVE-biV1H8Uju-0UEf25rXA2UuOaJw" controls controlsList="nodownload"/>
+            <audio src="https://docs.google.com/uc?export=download&id=1pMxVE-biV1H8Uju-0UEf25rXA2UuOaJw" controls controlsList="nodownload" />
             <p>Of What Is</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1j3YLUawKOe57x4s8L5eqGeBBKiKtnlPx" controls controlsList="nodownload"/>
+            <audio src="https://docs.google.com/uc?export=download&id=1j3YLUawKOe57x4s8L5eqGeBBKiKtnlPx" controls controlsList="nodownload" />
             <p>Near'r</p>
-            <audio src="https://docs.google.com/uc?export=download&id=11YaktPcPHyz_ClNm_WfIy7o6E3QKHRgZ" controls controlsList="nodownload"/>
+            <audio src="https://docs.google.com/uc?export=download&id=11YaktPcPHyz_ClNm_WfIy7o6E3QKHRgZ" controls controlsList="nodownload" />
             <p>Mended</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1NKLEsBqNHU7_1vii1xhL4NRsVSwO5p2y" controls controlsList="nodownload"/>
+            <audio src="https://docs.google.com/uc?export=download&id=1NKLEsBqNHU7_1vii1xhL4NRsVSwO5p2y" controls controlsList="nodownload" />
             <p>Binaural Octet for Solo Voice</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1a7a-g6TJvoNzHkiikhyohH6yx-sRHvTY" controls controlsList="nodownload"/>
+            <audio src="https://docs.google.com/uc?export=download&id=1a7a-g6TJvoNzHkiikhyohH6yx-sRHvTY" controls controlsList="nodownload" />
         </div>
     )
 }
