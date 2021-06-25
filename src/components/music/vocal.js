1352d28d30a1112ac066a10a9d9a563d6b6775d6
Blake DeGraw <bbdegraw@gmail.com>
2021-06-25 11:56:34 -0700

    calling it an afternoon for this. next step, store the music ids in state and render a given song's audio stream whenever you click the title

--- src/components/music/vocal.js
+++ src/components/music/vocal.js
@@ -1,12 +1,23 @@
 import Nav from '../nav'
 import Modal from 'react-modal'
 
+const musicIds = [
+    '1H0Z7yH_fMVg4hf0cLDwh8Oo5VuXUGijL',
+    '1FAfS9VVeD1HiKBuZ-bKytR9W8-YkCmzW',
+    '1ikr3KtRIds5kw0iYhC0rM5zw2170VS7V',
+    '1pMxVE-biV1H8Uju-0UEf25rXA2UuOaJw',
+    '1j3YLUawKOe57x4s8L5eqGeBBKiKtnlPx',
+    '11YaktPcPHyz_ClNm_WfIy7o6E3QKHRgZ',
+    '1NKLEsBqNHU7_1vii1xhL4NRsVSwO5p2y',
+    '1a7a-g6TJvoNzHkiikhyohH6yx-sRHvTY'
+]
+
 function Vocal(props) {
     return (
         <div>
             {/* <button onClick={() => props.handleModal(true)}>Open Modal</button> */}
             <Modal isOpen={props.modalIsOpen} onRequestClose={() => props.handleModal(false)}>
-                <h2>{props.modalContent}</h2>
+                <audio src="https://docs.google.com/uc?export=download&id=1H0Z7yH_fMVg4hf0cLDwh8Oo5VuXUGijL" controls controlsList="nodownload" />
                 <button onClick={() => props.handleModal(false)}>close</button>
             </Modal>
             <Nav />
