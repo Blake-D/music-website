6d2cf21bab919a6cad8368946e46372e277a26c9
Blake DeGraw <bbdegraw@gmail.com>
2021-06-29 12:16:24 -0700

    got the vocals page setup to pass player information to the player page. all seems to be working correctly. but I'm going to take a stab at reworking the player route to take an :id for each track. refresh clears the player info in its current configuration

--- src/components/music/player.js
+++ src/components/music/player.js
@@ -1,6 +1,9 @@
+import Nav from '../nav'
+
 function Player(props){
     return(
         <div>
+            <Nav />
             <p>{props.playerInfo.title}</p>
             <audio src={props.playerInfo.url} controls controlsList="nodownload" />
         </div>
--- src/components/music/vocal.js
+++ src/components/music/vocal.js
@@ -17,19 +17,14 @@ function Vocal(props){
             {/* <p onClick={() => props.handleModal(true)}>Victims of the Rooster</p>
             <audio src="https://docs.google.com/uc?export=download&id=1H0Z7yH_fMVg4hf0cLDwh8Oo5VuXUGijL" controls controlsList="nodownload" /> */}
             <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=1H0Z7yH_fMVg4hf0cLDwh8Oo5VuXUGijL" onClick={(e) => props.handlePlayerInfo(e)}>Victims of the Rooster</Link><br></br>
-            <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=1FAfS9VVeD1HiKBuZ-bKytR9W8-YkCmzW" onClick={(e) => props.handlePlayerInfo(e)}>They</Link>
-            <p>They</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1FAfS9VVeD1HiKBuZ-bKytR9W8-YkCmzW" controls controlsList="nodownload" />
-            <p>Six Vowels</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1ikr3KtRIds5kw0iYhC0rM5zw2170VS7V" controls controlsList="nodownload" />
-            <p>Second Draft</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1pMxVE-biV1H8Uju-0UEf25rXA2UuOaJw" controls controlsList="nodownload" />
-            <p>Of What Is</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1j3YLUawKOe57x4s8L5eqGeBBKiKtnlPx" controls controlsList="nodownload" />
-            <p>Near'r</p>
-            <audio src="https://docs.google.com/uc?export=download&id=11YaktPcPHyz_ClNm_WfIy7o6E3QKHRgZ" controls controlsList="nodownload" />
-            <p>Mended</p>
-            <audio src="https://docs.google.com/uc?export=download&id=1NKLEsBqNHU7_1vii1xhL4NRsVSwO5p2y" controls controlsList="nodownload" />
+            <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=1FAfS9VVeD1HiKBuZ-bKytR9W8-YkCmzW" onClick={(e) => props.handlePlayerInfo(e)}>They</Link><br></br>
+            <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=1ikr3KtRIds5kw0iYhC0rM5zw2170VS7V" onClick={(e) => props.handlePlayerInfo(e)}>Six Vowels</Link><br></br>
+            <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=1pMxVE-biV1H8Uju-0UEf25rXA2UuOaJw" onClick={(e) => props.handlePlayerInfo(e)}>Second Draft</Link><br></br>
+            <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=1j3YLUawKOe57x4s8L5eqGeBBKiKtnlPx" onClick={(e) => props.handlePlayerInfo(e)}>Of What Is</Link><br></br>
+            <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=11YaktPcPHyz_ClNm_WfIy7o6E3QKHRgZ" onClick={(e) => props.handlePlayerInfo(e)}>Near'r</Link><br></br>
+            <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=1NKLEsBqNHU7_1vii1xhL4NRsVSwO5p2y" onClick={(e) => props.handlePlayerInfo(e)}>Mended</Link><br></br>
+            <Link to="/music/player" id="https://docs.google.com/uc?export=download&id=1a7a-g6TJvoNzHkiikhyohH6yx-sRHvTY" onClick={(e) => props.handlePlayerInfo(e)}>Binaural Octet for Solo Voice</Link><br></br>
+
             <p>Binaural Octet for Solo Voice</p>
             <audio src="https://docs.google.com/uc?export=download&id=1a7a-g6TJvoNzHkiikhyohH6yx-sRHvTY" controls controlsList="nodownload" />
         </div>
